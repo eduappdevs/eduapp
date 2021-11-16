d16a07489c9bce0086e1dc1b17066f597c2605d7
Cristina <eduappdevelopment@gmail.com>
2021-11-16 18:28:46 +0000

    16/11/21

--- frontend/src/views/resources/resources.js
+++ frontend/src/views/resources/resources.js
@@ -1,29 +1,31 @@
-import axios from 'axios'
 import React from 'react'
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import "./resources.css"
 
-let resources = []
-const getAllResources = async()=>{
-    resources = await axios.get("http://localhost:3000/resources").data
-}
 
 export default function Resources() {
-    
-    useEffect(() => {
-        return () => {
-            getAllResources()
-        }
-    }, [])
-    console.log(resources)
+    const [resources, setResources] = useState([])
+const getResources = async () => {
+    const response = await fetch(
+        "http://localhost:3000/resources"
+    );
+    const data = await response.json();
+    console.log(data);
+    setResources(data);
+};
+
+useEffect(() => {
+   getResources()
+}, [])
     return (
-        <div>
+        <div className="resources_container">
             <ul>
-            {resources.map((data)=>{
-                return <li>
-                    <h1>{data.name}</h1>
-                    <h1>{data.description}</h1>
-                </li>
-            })}
+                {resources.map((res)=> {
+                    return <div className="resource_item">
+                        <h1>{res.name}</h1>
+                        <h1>{res.description}</h1>
+                    </div>    
+                })}
             </ul>
         </div>
     )
