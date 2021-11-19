483d5af6535c6cb397c4ae968332230066df2584
adriceniza <ceniiziienta@gmail.com>
2021-11-19 11:40:21 +0000

    commit develop 19/11/21

--- frontend/src/views/resources/resources.js
+++ frontend/src/views/resources/resources.js
@@ -1,30 +1,34 @@
-import axios from 'axios'
-import React from 'react'
-import {useEffect} from 'react'
-
-let resources = []
-const getAllResources = async()=>{
-    resources = await axios.get("http://localhost:3000/resources").data
-}
-
+import React from "react";
+import { useEffect, useState } from "react";
+import Navbar from "../../components/navbar/navbar";
+import "./resources.css";
 export default function Resources() {
-    
-    useEffect(() => {
-        return () => {
-            getAllResources()
-        }
-    }, [])
-    console.log(resources)
-    return (
-        <div>
-            <ul>
-            {resources.map((data)=>{
-                return <li>
-                    <h1>{data.name}</h1>
-                    <h1>{data.description}</h1>
-                </li>
-            })}
-            </ul>
-        </div>
-    )
+  const [resources, setResources] = useState([]);
+  const getResources = async () => {
+    const response = await fetch("http://localhost:3000/resources");
+    const data = await response.json();
+    console.log(data);
+    setResources(data);
+  };
+
+  useEffect(() => {
+    getResources();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div className="resources-container">
+        <ul>
+          {resources.map((data) => {
+            return (
+              <li>
+                <h1>{data.name}</h1>
+                <h1>{data.description}</h1>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
