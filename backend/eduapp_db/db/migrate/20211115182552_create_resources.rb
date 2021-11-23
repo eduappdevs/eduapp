98bfefdd9740a7dfd8bfb66b8bd75870e97b8f00
Cristina <eduappdevelopment@gmail.com>
2021-11-23 16:00:45 +0000

    Home

--- backend/eduapp_db/db/migrate/20211115182552_create_resources.rb
+++ backend/eduapp_db/db/migrate/20211115182552_create_resources.rb
@@ -4,6 +4,7 @@ class CreateResources < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :description
 
+
       t.timestamps
     end
   end
--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -5,7 +5,18 @@ import BottomButtons from "../../components/bottomButtons/bottomButtons";
 import "./home.css";
 
 export default function Home() {
+  const [ItsMobileDevice, setItsMobileDevice] = useState(false);
   const [sessions, setSessions] = useState([]);
+  const [sessionOpened, setSessionOpened] = useState(false);
+  const checkMediaQueries = () => {
+    setInterval(() => {
+      if (window.matchMedia("(max-width: 1000px)").matches) {
+        setItsMobileDevice(true);
+      } else {
+        setItsMobileDevice(false);
+      }
+    }, 4000);
+  };
   const getSessions = async () => {
     const session = [
       {
@@ -33,74 +44,77 @@ export default function Home() {
     const data = await session;
     setSessions(data);
   };
+  const openSession = (e) =>{
+    e.preventDefault();
+    setSessionOpened(true);
+    setTimeout(() => {
+      try {
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }, 100);
+  }
   useEffect(() => {
     getSessions();
+    checkMediaQueries();
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+      setItsMobileDevice(true);
+    } else {
+      setItsMobileDevice(false);
+    }
   }, []);
   return (
     <>
       <Navbar />
       <div className="user">
-        <div className="information-user">
-          <div className="profile-picture">
-            <img
-              src="https://img-aws.ehowcdn.com/600x400/photos.demandstudios.com/getty/article/34/162/91911271.jpg?type=webp"
-              alt=""
-            />
-          </div>
-          <div className="user-name">
-            <h1>Pepe</h1>
-            <div className="edit">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                class="bi bi-pencil-square"
-                viewBox="0 0 16 16"
-              >
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                <path
-                  fill-rule="evenodd"
-                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                />
-              </svg>
+        <div className="user-container">
+          <div className="information-user">
+            <div className="profile-picture">
+              <img
+                src="https://img-aws.ehowcdn.com/600x400/photos.demandstudios.com/getty/article/34/162/91911271.jpg?type=webp"
+                alt=""
+              />
+            </div>
+            <div className="user-name">
+              <h1>Richard Clarke</h1>
+              <div className="edit">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M1.438 16.872l-1.438 7.128 7.127-1.438 12.642-12.64-5.69-5.69-12.641 12.64zm2.271 2.253l-.85-.849 11.141-11.125.849.849-11.14 11.125zm20.291-13.436l-2.817 2.819-5.69-5.691 2.816-2.817 5.691 5.689z" />
+                </svg>
+              </div>
             </div>
           </div>
           <div className="sessions">
-            {/* <div className="title-first-session">
-              <p>Next Session</p>
-            </div>
-            <div className="sessions-item">
-              <div className="button-information">
-                <svg xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                </svg>
-              </div> */}
             {sessions.map((data) => {
               return (
                 <>
-                  <div className="sessions-container">
-                    <div id={data.id} className="sessions-closed hidden">
+                  <div className="sessions-container" id={"ses"+data.id}>
+                    <div className="sessions-closed ">
+                      <div className="session-before">
+          
+                      <div id={"ses"+data.id} className="arrow" onClick={openSession}>
                       <svg xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
                       </svg>
-
-                      <span className="session-name">{data.name}</span>
-                      <span className="session-date">{data.date}</span>
-                    </div>
-                    <div className="sessions-openned ">
-                      <svg xmlns="http://www.w3.org/2000/svg">
-                        <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                      </svg>
-                      <span className="session-name">{data.name}</span>
-                      <span className="session-date">{data.date}</span>
-                      <div className="session-description">
-                        <p className="session-streamingPlatform">
-                          {data.streamingPlatform}
-                        </p>
-                        <p className="session-resourcesPlatform">
-                          {data.resourcesPlatform}
-                        </p>
+                      </div>
+                        <span className="session-name">{data.name}</span>
+                        <span className="session-date">{data.date}</span>
+                      </div>
+                      <div  className="sessions-after hidden">
+                        <div className="session-description">
+                          <p className="session-streamingPlatform">
+                            {data.streamingPlatform}
+                          </p>
+                          <p className="session-resourcesPlatform">
+                            {data.resourcesPlatform}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -112,7 +126,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <BottomButtons location={"home"} />
+      <BottomButtons mobile={ItsMobileDevice} location={"home"} />
     </>
   );
 }
