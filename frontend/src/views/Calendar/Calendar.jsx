7c946e36a1b8c24179c4aec4548f514a56b3653c
unknown <cristinasantanamartin2001@gmail.com>
2022-02-28 01:08:59 +0000

    solve some problems in calendar events view

--- frontend/src/views/Calendar/Calendar.jsx
+++ frontend/src/views/Calendar/Calendar.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
-import Paper from "@material-ui/core/Paper";
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@material-ui/core/Paper';
 import {
   Scheduler,
   DateNavigator,
@@ -13,12 +13,12 @@ import {
   ViewSwitcher,
   TodayButton,
   AppointmentTooltip,
-} from "@devexpress/dx-react-scheduler-material-ui";
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import { CALENDAR_USER_ID, SUBJECT } from "../../config";
-import View from "./eventsView/View";
-import CreateView from "./eventsView/CreateView";
-import "./calendar.css";
+} from '@devexpress/dx-react-scheduler-material-ui';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import { CALENDAR_USER_ID, SUBJECT } from '../../config';
+import View from './eventsView/View';
+import CreateView from './eventsView/CreateView';
+import './calendar.css';
 
 export default function Calendar() {
   const [annotations, setAnnotations] = useState([]);
@@ -33,9 +33,9 @@ export default function Calendar() {
     let events = [];
     let annotations = await axios.get(CALENDAR_USER_ID + localStorage.userId);
     let data = annotations.data;
+    console.log(data);
 
     for (let globalEvent in data.globalEvents) {
-      console.log(globalEvent);
       if (data.globalEvents !== null) {
         let e = data.globalEvents[globalEvent];
         let id = e.id;
@@ -65,6 +65,7 @@ export default function Calendar() {
 
     for (let calendarEvent in data.calendarEvents) {
       if (data.calendarEvents !== null) {
+        console.log(data.calendarEvents);
         let calendarEvents = data.calendarEvents[calendarEvent];
         let id = calendarEvents.id;
         let startDate = calendarEvents.annotation_start_date;
@@ -125,10 +126,11 @@ export default function Calendar() {
       }
     }
     setAnnotations(events);
+    console.log(annotations);
   };
 
   const getSubject = async () => {
-    let request = await axios.get(SUBJECT + "?user_id=" + localStorage.user_id);
+    let request = await axios.get(SUBJECT + '?user_id=' + localStorage.user_id);
     let subject = [];
     request.data.map((e) => {
       let id = e.id;
@@ -142,9 +144,9 @@ export default function Calendar() {
     setSubject(subject);
   };
 
-  const getCalendarEvent = (eventId) => {
+  const getCalendarEvent = (data) => {
     for (let event of annotations) {
-      if (event.id === eventId) {
+      if (event === data) {
         setActiveEvent(event);
         break;
       }
@@ -152,56 +154,56 @@ export default function Calendar() {
   };
 
   const openCreate = async () => {
-    const chatBox = document.getElementById("create-box");
-    chatBox.style.display = "flex";
+    const chatBox = document.getElementById('create-box');
+    chatBox.style.display = 'flex';
     const backgroundCalendar =
-      document.getElementsByClassName("background-shadow")[0];
+      document.getElementsByClassName('background-shadow')[0];
     const calendarMainScroll = document.getElementsByClassName(
-      "calendar-main-container"
+      'calendar-main-container'
     )[0];
 
     setTimeout(() => {
-      calendarMainScroll.classList.add("disable-scroll");
-      backgroundCalendar.style.display = "block";
+      calendarMainScroll.classList.add('disable-scroll');
+      backgroundCalendar.style.display = 'block';
     }, 200);
 
     setTimeout(() => {
-      chatBox.classList.add("create-box-opened");
-      chatBox.classList.remove("calendar-view-create-hidden");
+      chatBox.classList.add('create-box-opened');
+      chatBox.classList.remove('calendar-view-create-hidden');
     }, 400);
   };
 
-  const showEventView = async (eventId) => {
-    getCalendarEvent(eventId);
+  const showEventView = async (data) => {
+    getCalendarEvent(data);
 
-    const viewBox = document.getElementById("view-box");
-    viewBox.style.display = "flex";
+    const viewBox = document.getElementById('view-box');
+    viewBox.style.display = 'flex';
     const backgroundCalendar =
-      document.getElementsByClassName("background-shadow")[0];
+      document.getElementsByClassName('background-shadow')[0];
     const calendarMainScroll = document.getElementsByClassName(
-      "calendar-main-container"
+      'calendar-main-container'
     )[0];
 
     setTimeout(() => {
-      viewBox.classList.remove("view-box-closed");
-      viewBox.classList.add("view-box-opened");
+      viewBox.classList.remove('view-box-closed');
+      viewBox.classList.add('view-box-opened');
       document
-        .getElementsByClassName("calendar-view-main-container")[0]
-        .classList.remove("calendar-view-hidden");
+        .getElementsByClassName('calendar-view-main-container')[0]
+        .classList.remove('calendar-view-hidden');
     }, 1);
 
     setTimeout(() => {
-      calendarMainScroll.classList.add("disable-scroll");
-      backgroundCalendar.style.display = "block";
+      calendarMainScroll.classList.add('disable-scroll');
+      backgroundCalendar.style.display = 'block';
     }, 500);
   };
 
-  const StyledDiv = styled("div")(({ theme }) => ({}));
+  const StyledDiv = styled('div')(({ theme }) => ({}));
   const Appointment = ({ data, children, style, ...restProps }) => (
     <StyledDiv
       className={`event_${data.id}`}
       onClick={() => {
-        showEventView(data.id);
+        showEventView(data);
       }}
     >
       <Appointments.Appointment
@@ -209,7 +211,7 @@ export default function Calendar() {
         style={{
           ...style,
           backgroundColor: data.backgroundColor,
-          borderRadius: "8px",
+          borderRadius: '8px',
         }}
       >
         {children}
@@ -244,8 +246,8 @@ export default function Calendar() {
       <section
         className={
           ItsMobileDevice
-            ? "mobileSection calendar-main-section"
-            : "desktopSection calendar-main-section"
+            ? 'mobileSection calendar-main-section'
+            : 'desktopSection calendar-main-section'
         }
       >
         <div className="calendar">
