da4fb2118acb9de9281a43111e56af9f5a0abfb7
SkinnyDevi <xthesmartdartx@gmail.com>
2022-04-28 15:09:53 +0100

    Add a standard modal for app pop-ups

---
+++ frontend/src/components/modals/standard-modal/StandardModal.jsx
@@ -0,0 +1,152 @@
+import "./StandardModal.css";
+
+export default function StandardModal({
+  text,
+  type,
+  show,
+  iconFill,
+  iconColor,
+  isQuestion,
+  onYesAction,
+  onNoAction,
+  closeAction,
+  hasTransition,
+  hasIconAnimation,
+}) {
+  // TYPES = ['success', 'error', 'warning', 'info']
+  return (
+    <div
+      className={`standard-modal-container ${
+        hasTransition ? "standard-modal-transition-bg" : ""
+      }`}
+      style={{ display: show ? "flex" : "none" }}
+    >
+      <div
+        className={`standard-modal ${hasIconAnimation ? "svg-animation" : ""} ${
+          hasTransition ? "standard-modal-transition" : ""
+        }`}
+      >
+        {show && (
+          <div>
+            {iconFill && type === "success" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#0d750d"}
+                className="bi bi-check-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              </svg>
+            )}
+            {!iconFill && type === "success" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#0d750d"}
+                className="bi bi-check-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+              </svg>
+            )}
+            {iconFill && type === "warning" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#f76b00"}
+                className="bi bi-exclamation-diamond-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+              </svg>
+            )}
+            {!iconFill && type === "warning" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#f76b00"}
+                className="bi bi-exclamation-diamond"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+              </svg>
+            )}
+            {iconFill && type === "error" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#bd0003"}
+                className="bi bi-exclamation-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+              </svg>
+            )}
+            {!iconFill && type === "error" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#bd0003"}
+                className="bi bi-exclamation-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+              </svg>
+            )}
+            {iconFill && type === "info" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#00b4d4"}
+                className="bi bi-info-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+              </svg>
+            )}
+            {!iconFill && type === "info" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill={iconColor ? iconColor : "#00b4d4"}
+                className="bi bi-info-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
+            )}
+          </div>
+        )}
+        <h3>{text}</h3>
+
+        {isQuestion ? (
+          <div className="standard-modal-btn-container">
+            <div className="standard-modal-btn" onClick={onYesAction}>
+              Yes
+            </div>
+            <div className="standard-modal-btn" onClick={onNoAction}>
+              No
+            </div>
+          </div>
+        ) : (
+          <div className="standard-modal-btn" onClick={closeAction}>
+            Okay
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
