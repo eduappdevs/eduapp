430964124db6ab235075af71ec78f05f41520a63
SkinnyDevi <xthesmartdartx@gmail.com>
2022-05-06 10:41:43 +0100

    Fixed hooks for security pt .2

--- frontend/src/hooks/GetSubjects.jsx
+++ frontend/src/hooks/GetSubjects.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
-import API from "../API";
+import * as SUBJECT_SERVICE from "../services/subject.service";
+import { getOfflineUser } from "../utils/OfflineManager";
 
 export const GetSubjects = (id) => {
   const [subject, setSubject] = useState([]);
 
   useEffect(() => {
     const getSubjects = async () => {
-      if (navigator.onLine) {
+      if (navigator.onLine && getOfflineUser().user !== null) {
         try {
-          const subject = await API.getSubjects(id);
+          const subject = (await SUBJECT_SERVICE.fetchUserVariantSubjects(id))
+            .data;
           setSubject([...subject]);
         } catch (error) {
           console.log(error);
