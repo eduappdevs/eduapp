10848092ca5174b102d9e925e651b7d6a6932a1e
SkinnyDevi <xthesmartdartx@gmail.com>
2022-05-18 16:14:51 +0100

    Added custom library for encrypting messages

---
+++ backend/eduapp_db/lib/edu_app_utils/encrypt_utils.rb
@@ -0,0 +1,36 @@
+module EduAppUtils
+	module EncryptUtils
+		require 'openssl'
+		require 'securerandom'
+
+		@salt = Base64.decode64(ENV.fetch("ENCRYPTION_SALT"))
+		@pattern = ENV.fetch("ENCRYPTION_PATTERN")
+
+		def self.gen_key_pair(hashed_pwd)
+			key = OpenSSL::PKey::RSA.generate(2048)
+
+			pri_key = OpenSSL::PKey::RSA.new(key, "#{hashed_pwd}::#{SecureRandom.random_bytes(32)}")
+			pub_key = OpenSSL::PKey::RSA.new(key.public_key)
+
+			encrypted = self.encrypt("nice", pub_key)
+			puts "Encrypted msg: #{encrypted}"
+			puts "Decrypted msg: #{self.decrypt(encrypted, pri_key)}"
+		end
+
+		def self.encrypt(msg, pub_key)
+			return Base64.encode64("#{@salt}#{pattern}#{pub_key.public_encrypt(msg)}")
+		end
+
+		def self.decrypt(cipher_text, pri_key)
+			begin
+				return pri_key.private_decrypt(Base64.decode64(cipher_text).split(pattern)[1])
+			rescue OpenSSL::PKey::RSAError => e
+				puts "Invalid decryption: #{e}"
+				return nil
+			rescue TypeError => e
+				puts "Message tampered! No salt provided: #{e}"
+				return nil
+			end
+		end
+	end
+end
