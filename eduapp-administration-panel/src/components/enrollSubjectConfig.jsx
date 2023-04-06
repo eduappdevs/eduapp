0aaa2e2cb87338ed1239363482e7bb17606b89ff
tcrurav <tiburcio.cruz@gmail.com>
2023-04-06 19:48:14 +0100

    FIX Enrollment (Subjects)

--- eduapp-administration-panel/src/components/enrollSubjectConfig.jsx
+++ eduapp-administration-panel/src/components/enrollSubjectConfig.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Fragment, useContext, useEffect, useState, useRef } from "react";
 import * as API from "../API";
-// import * as SUBJECTSUSERSSERVICE from "../services/enrollSubjectConfig.service";
+import * as SUBJECTSUSERSSERVICE from "../services/enrollSubjectConfig.service";
 import * as USERSSERVICE from "../services/user.service";
 import * as SUBJECTSERVICE from "../services/subject.service";
 import * as CHATSERVICE from "../services/chat.service";
@@ -65,7 +65,7 @@ export default function EnrollSubjectConfig() {
 
   const fetchSubjectsUsers = (pages) => {
     API.asynchronizeRequest(function () {
-      SUBJECTSERVICE.pagedSubjects(pages, searchParams)
+      SUBJECTSUSERSSERVICE.pagedSubjectsUsers(pages, searchParams)
         .then((ts) => {
           setSubjectsUsers(ts.data.current_page);
           setActualPage(ts.data.page);
@@ -227,44 +227,45 @@ export default function EnrollSubjectConfig() {
     return (
       <>
         {
-          subjectsUsers && subjectsUsers.filter(subject => subject.users).map((subject) => {
-            return subject.users.map((user, index) => {
-              // subjectsUsers && subjectsUsers.map((su, index) => { 
-              return (
-                <tr key={user.id}>
-                  <td>
-                    { user.email }
-                  </td>
-                  <td>
-                    { subjects && subjects.find(s => s.id === subject.id)?.name }
-                  </td>
-                  <td style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
+          // subjectsUsers && subjectsUsers.filter(subject => subject.users).map((subject) => {
+          //   return subject.users.map((user, index) => {
+          subjectsUsers && subjectsUsers.map((su, index) => {
+            return (
+              <tr key={su.id}>
+                <td>
+                  {su.user.email}
+                </td>
+                <td>
+                  {/* {subjects && subjects.find(s => s.id === subject.id)?.name} */}
+                  {su.subject.name}
+                </td>
+                <td style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                >
+                  <button
+                    style={{ marginRight: "5px" }}
+                    onClick={() => confirmDeleteEvent(su.subject, su.user.id)}
                   >
-                    <button
-                      style={{ marginRight: "5px" }}
-                      onClick={() => confirmDeleteEvent(subject, user.id)}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash3"
+                      viewBox="0 0 16 16"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-trash3"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              )
-            })
+                      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            )
           })
         }
+        {/* } */}
       </>
     )
   }
