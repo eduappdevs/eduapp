065420b68c5e7aab54de1ebe03c0ec8d62f376d6
tcrurav <tiburcio.cruz@gmail.com>
2023-06-14 11:44:43 +0100

    FIX ios home screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import PWAPrompt from 'react-ios-pwa-prompt';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <PWAPrompt promptOnVisit={1} timesToShow={3} copyClosePrompt="Close" permanentlyHideOnDismiss={false}/>
   </React.StrictMode>,
   document.getElementById("root")
 );
